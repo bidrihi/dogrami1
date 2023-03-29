ac169042317e8ec96903dd5d239257ec6f0522c9
bidrihi <bidrihi@gmail.com>
2023-03-29 10:13:25 +0900

    commit

--- src/main/java/com/cono/dogrami/quiz/model/dao/QuizDao.java
+++ src/main/java/com/cono/dogrami/quiz/model/dao/QuizDao.java
@@ -1,5 +1,7 @@
 package com.cono.dogrami.quiz.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.cono.dogrami.quiz.model.vo.Quiz;
